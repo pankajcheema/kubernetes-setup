b88da0b614de34232217fdecb7c1c2ffcd7464d8
pankaj <pankajcheema1990@gmail.com>
2019-02-18 17:32:36 +0530

    working setup node js and mongo kubernetes

--- setup.sh
+++ setup.sh
@@ -1,25 +1,25 @@
-#!bin/bsh
+#! /bin/bash
 echo "Generating persistence volumes for mongodb size 10 gb>>>>>>>>>>>>>>>>"
 
-kubectl $0 -f ./mongo_persistence.yaml
+kubectl $1 -f ./mongo_persistence.yaml
 sleep 3
 
 echo "Generating persistence volumes claim for 3 gb for mongodb>>>>>>>>>>>>>>>>"
-kubectl $0 -f ./mongo_persistence_volumeclain.yaml
+kubectl $1 -f ./mongo_persistence_volumeclain.yaml
 
 echo "creating deployments for mongo db>>>>>>>>>>>>>>>>"
 
-kubectl $0 -f ./mongo_deployment.yaml
+kubectl $1 -f ./mongo_deployment.yaml
 
 echo "creating service to expose outside  for mongo db>>>>>>>>>>>>>>>>"
 
-kubectl $0 -f ./mongo_service.yaml
+kubectl $1 -f ./mongo_service.yaml
 
 echo "creating node js deployments>>>>>>>>>>>>>>>>"
 
-kubectl $0 -f ./nodejs_deployment.yaml
+kubectl $1 -f ./nodejs_deployment.yaml
 
 echo "creating node js service>>>>>>>>>>>>>>>>"
 
 
-kubectl $0 -f ./node_service.yaml
+kubectl $1 -f ./node_service.yaml
